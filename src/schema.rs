e818e255499b242885b32b7de9d6d5887e1acf05
Hardik <hardikbhimani@rocketmail.com>
2024-02-18 16:14:02 +0530

    Completed users CRUD

--- src/schema.rs
+++ src/schema.rs
@@ -1,3 +1,4 @@
+use chrono;
 use std::usize;
 
 use serde::{Deserialize, Serialize};
@@ -8,6 +9,14 @@ pub struct CreateUserSchema {
     pub last_name: Option<String>,
     pub email: String,
     pub password: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UpdateUserSchema {
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub email: String,
+    pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
